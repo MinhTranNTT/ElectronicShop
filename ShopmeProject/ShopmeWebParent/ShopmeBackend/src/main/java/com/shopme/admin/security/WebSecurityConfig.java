669a347d234d42e131cb389879aea42bbf86fc62
MinhTranNTT <tvhuuminhntt@gmail.com>
2022-03-14 20:21:47 +0700

    Code User authorization basic

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -29,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/users/**").hasAuthority("Admin")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
